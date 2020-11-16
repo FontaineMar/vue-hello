f132c1d598a565efeab7da3c972bd2c13f6b5622
marco <odri.fo@gmail.com>
2020-11-16 17:24:16 +0100

    quasi finito

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var descrizione = new Vue({
+  el: '#myApp',
+  data:{
+    questionName:'Come ti chiami?',
+    name:'',
+    questionSurname: 'Qual è il tuo cognome?',
+    surname:'',
+    questionAge: 'Quanti anni hai?',
+    age:'',
+    questionHobby:'Cosa ti piace fare nel tempo libero?',
+    hobby:'',
+    immagine:'../img.jpg'
+
+  },
+
+})
